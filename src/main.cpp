3ca03c70142efb0225aa217dd0dc5dfc4df6f7d7
Stephan Zehnder <stephan.zehnder@ipt.ch>
2022-11-19 13:50:43 +0100

    FEATURE: Added basic Webserver

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <chrono>
 #include <unistd.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 using namespace std;
 
 #define START 22
@@ -21,6 +24,62 @@ using namespace std;
 
 int ADL[] = {25, 26, 27, 4};
 int ADC[] = {21, 19, 18, 17, 16};
+
+// Set up Webserver
+AsyncWebServer server(80);
+const char* ssid = "zimtbaum";
+const char* password = "Zipfel-Muetze";
+
+// HTML web page to handle input field
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/get">
+    input1: <input type="text" name="input1">
+    <input type="submit" value="Submit">
+  </form><br>
+</body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+void setupWebserver() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    inputMessage = request->getParam("input1")->value();
+
+    Serial.println(inputMessage);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+
+
+
 
 // l: 1, 2, 3 or 4
 // val: HIGH or LOW
@@ -61,6 +120,8 @@ void deselectAllModules() {
 
 void setup() {
   Serial.begin(115200);
+  setupWebserver();
+  
   pinMode(START, OUTPUT);
   for (int i=0; i<=3; i++) {
     pinMode(ADL[i], OUTPUT);
@@ -141,7 +202,7 @@ char getPrecedingCharacter(char myChar) {
 
 
 void loop() {
-  string draft[] = {"ijkl"};
+  string draft[] = {"mnlo"};
   int numOfRows = size(draft);
   string *text = createText(draft, numOfRows);
 
