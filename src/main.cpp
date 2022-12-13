a18f65b4e29a8b90e45f201508c38a927b6b50bf
Stephan Zehnder <79573800+cyber-moon@users.noreply.github.com>
2022-12-13 21:40:26 +0100

    Feature/webserver (#2)
    * FEATURE: Added basic Webserver
    
    * FEATURE: Connect Webserver to Flapflap
    
    * INTERNAL: Improved robustnes
    
    * FEATURE: Improved Stability when using multiple rows
    
    * FEATURE: Check character correctness
    
    * FEATURE: Get it to work with 2x5 modules!
    
    * FEATURE: Improved Webinterface, available on flap.local
    
    * FEATURE: Updated to 30 chars & Removed NumOfRows-Bug
    
    * INTERNAL: Cleaned up code
    
    * FEATURE: Upgraded to 13 characters per line

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #include <iostream>
 #include <chrono>
 #include <unistd.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 using namespace std;
 
 #define START 22
@@ -21,6 +25,103 @@ using namespace std;
 
 int ADL[] = {25, 26, 27, 4};
 int ADC[] = {21, 19, 18, 17, 16};
+char supportedCharacters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','V','O','P','Q','R','S','T','U','V','W','X','Y','Z','/','-','1','2','3','4','5','6','7','8','9','0','.',' '};
+
+string draft[] = {"        ", "flap.local", "        "};
+
+int numOfRows = 3;
+int numOfCols = 13;
+
+// Set up Webserver
+AsyncWebServer server(80);
+const char* ssid = "zimtbaum";
+const char* password = "Zipfel-Muetze";
+
+// HTML web page to handle input field
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>flapflap</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <style type="text/css">
+        #btn_s{
+            width:100px;
+        }
+
+        #btn_i {
+            width:125px;
+        }
+        #formbox {
+            margin:auto 0;
+            text-align: center;
+        }
+    </style>
+  </head>
+	<body>
+		<form action="/get">
+      <div id="formbox">
+        <br>
+        <input type="text" name="line1" maxlength="13" style="font-family: 'Courier New'; font-size : 28px; height:40px; width:260px"><br />
+        <br>
+        <input type="text" name="line2" maxlength="13" style="font-family: 'Courier New'; font-size : 28px; height:40px; width:260px"><br />
+        <br>
+        <input type="text" name="line3" maxlength="13" style="font-family: 'Courier New'; font-size : 28px; height:40px; width:260px"><br />
+        <br>
+        <input type="submit" value="flapflap" style="font-size : 35px; height:50px; width:150px">
+      </div>
+		</form><br>
+    <center>
+      Max. 13 Zeichen pro Zeile. <br>
+      Erlaubte Zeichen: 'A'-'Z', '0'-'9', '.', '/', '-', ' '
+    </center>
+	</body>
+</html>
+)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+void setupWebserver() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  if(!MDNS.begin("flap")) {
+    Serial.println("Error starting mDNS");
+    return;
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  string ipAdress = WiFi.localIP().toString().c_str();
+  cout << ipAdress << endl;
+
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Send a GET request to <ESP_IP>/get?input=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    // GET input value on <ESP_IP>/get?input=<inputMessage>
+    String line1Message = request->getParam("line1")->value();
+    String line2Message = request->getParam("line2")->value();
+    String line3Message = request->getParam("line3")->value();
+    draft[0] = line1Message.c_str();
+    draft[1] = line2Message.c_str();
+    draft[2] = line3Message.c_str();
+    request->redirect("/");
+  });
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+
+
 
 // l: 1, 2, 3 or 4
 // val: HIGH or LOW
@@ -54,13 +155,16 @@ void deselectAllModules() {
   for (int i=0; i<=3; i++) {
     digitalWrite(ADL[i], LOW);
   }
-  for (int i=0; i<=4; i++) {
+  for (int i=0; i<=3; i++) {
     digitalWrite(ADC[i], LOW);
   }
+  digitalWrite(ADC[4], HIGH);
 }
 
 void setup() {
   Serial.begin(115200);
+  setupWebserver();
+
   pinMode(START, OUTPUT);
   for (int i=0; i<=3; i++) {
     pinMode(ADL[i], OUTPUT);
@@ -110,30 +214,28 @@ char getCurrentChar() {
   return '+';
 }
 
-int numOfCols = 4;
+string reviseText(string text) {
+  for (int j=0; j<size(text); j++) {
+    text[j] = toupper(text[j]);
+    if (find(supportedCharacters, supportedCharacters+size(supportedCharacters), text[j]) == (supportedCharacters+size(supportedCharacters))) {
+      cout << "Unsupported character: " << text[j] << endl;
+      text[j] = '/';
+    }
 
-string* createText(string* text, int numOfRows) {
-  for (int i=0; i<numOfRows; i++) {
-    string line = text[i];
-    for (int j=0; j<size(line); j++) {
-      line[j] = toupper(line[j]);
-    }
-    if (size(line) < numOfCols) {
-      int missingSpaces = numOfCols - size(line);
-      char spaces[missingSpaces];
-      fill_n(spaces, missingSpaces, ' ');
-      line = line.append(spaces);
-    }
-    text[i] = line;
+  }
+  if (size(text) < numOfCols) {
+    int missingSpaces = numOfCols - size(text);
+    char spaces[missingSpaces];
+    fill_n(spaces, missingSpaces, ' ');
+    text = text.append(spaces);
   }
   return text;
 }
 
 char getPrecedingCharacter(char myChar) {
-  char characters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','V','O','P','Q','R','S','T','U','V','W','X','Y','Z','/','-','1','2','3','4','5','6','7','8','9','0','.',' '};
-  char* position = find(characters, characters+size(characters), myChar);
+  char* position = find(supportedCharacters, supportedCharacters+size(supportedCharacters), myChar);
   char precedingChar = *(position-1);
-  if (position == characters) {
+  if (position == supportedCharacters) {
     precedingChar = ' ';
   }
   return precedingChar;
@@ -141,22 +243,30 @@ char getPrecedingCharacter(char myChar) {
 
 
 void loop() {
-  string draft[] = {"ijkl"};
-  int numOfRows = size(draft);
-  string *text = createText(draft, numOfRows);
-
-  char lastOutputChar[numOfRows * numOfCols];
-  bool isCorrect[numOfRows * numOfCols];
-  for (int i=0; i<numOfRows * numOfCols; i++) {
-    isCorrect[i] = false;
-  }
+  string oldDraft[] = {"", "", ""};
+  string text[] = {"", "", ""};
+  string lastOutputChars[numOfRows * numOfCols];
+  int isCorrect[numOfRows * numOfCols];
 
   // Iterate through the Matrix (Row-wise)
   while(1) {
+    if (oldDraft[0] != draft[0] || oldDraft[1] != draft[1] || oldDraft[2] != draft[2]) {
+      text[0] = reviseText(draft[0]);
+      text[1] = reviseText(draft[1]);
+      text[2] = reviseText(draft[2]);
+      oldDraft[0] = draft[0];
+      oldDraft[1] = draft[1];
+      oldDraft[2] = draft[2];
+      for (int i=0; i<numOfRows * numOfCols; i++) {
+        lastOutputChars[i] = "";
+        isCorrect[i] = 0;
+      }
+    }
+
     for (int i=0; i<numOfRows; i++) {
       string line = text[i];
       for (int j=0; j<numOfCols; j++) {
-        // if (!isCorrect[2*i + j]) {
+        if (isCorrect[numOfCols*i + j] < 10) {
           char myChar = line[j];
           char precedingChar = getPrecedingCharacter(myChar);
 
@@ -166,26 +276,21 @@ void loop() {
           selectADC(j);
           setADL(i+1, HIGH);
 
-          // if a valid character was detected
+          // Stop if myChar is found
           char currentChar = getCurrentChar();
-          // if(currentChar == myChar && lastOutputChar[2*i + j] == precedingChar) {
           if(currentChar == myChar) {
             digitalWrite(START, LOW);
-            isCorrect[2*i + j] = true;
+            isCorrect[numOfCols*i + j]++;
           } else {
             digitalWrite(START, HIGH);
-          }
-          if (currentChar != '+') {
-            if (currentChar != lastOutputChar[2*i + j]) {
-              cout << "Row: " << i << " Col: " << j << " Should: " << myChar << " Is: " << currentChar << endl;
-              lastOutputChar[2*i + j] = currentChar;
-            }
+            isCorrect[numOfCols*i + j] = 0;
           }
 
           // Un-select the module, so it continues turning (if character was not found)
           usleep(5);
           setADL(i+1, LOW);
-        // }
+          selectADC(31);
+        }
       }
     } 
   }
