b7ddf50e40bf5ef53a3d4829e65711484308dd93
Stephan Zehnder <79573800+cyber-moon@users.noreply.github.com>
2022-12-16 11:53:06 +0100

    Internal/cleanup (#3)
    * INTERNAL: Moved webserver-code to seperate file
    
    * INTERNAL: Changed draft-variables to vector
    
    * FEATURE: Correctly save last status of flapflap on restart
    
    * FEATURE: Added more buttons to webpage
    
    * FEATURE: Moved from GET to POST request
    
    * INTERNAL: Finished cleaning up Webserver.hpp
    
    * INTERNAL: Cleanup
    
    * INTERNAL: simplify access to DATA IO Pins
    
    * INTERNAL: Remove unnecessary parts
    
    * INTERNAL: Improve Comments
    
    * INTERNAL: More comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,160 +7,53 @@
 #include <Arduino.h>
 #include <string>
 #include <iostream>
-#include <chrono>
 #include <unistd.h>
-#include <ESPmDNS.h>
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
+#include <webserver.hpp>
 using namespace std;
 
-#define START 22
-#define DATA_0 32
-#define DATA_1 33
-#define DATA_2 34
-#define DATA_3 35
-#define DATA_4 36
-#define DATA_5 39
-
-int ADL[] = {25, 26, 27, 4};
-int ADC[] = {21, 19, 18, 17, 16};
-char supportedCharacters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','V','O','P','Q','R','S','T','U','V','W','X','Y','Z','/','-','1','2','3','4','5','6','7','8','9','0','.',' '};
-
-string draft[] = {"        ", "flap.local", "        "};
-
+// Define Screen Size
 int numOfRows = 3;
 int numOfCols = 13;
 
-// Set up Webserver
-AsyncWebServer server(80);
-const char* ssid = "zimtbaum";
-const char* password = "Zipfel-Muetze";
+// Define IO-Pins
+int START = 22;
+int ADL[] = {25, 26, 27, 4};
+int ADC[] = {21, 19, 18, 17, 16};
+int DATA[] = {32, 33, 34, 35, 36, 39};
+char supportedCharacters[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','V','O','P','Q','R','S','T','U','V','W','X','Y','Z','/','-','1','2','3','4','5','6','7','8','9','0','.',' '};
 
-// HTML web page to handle input field
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>flapflap</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-    <style type="text/css">
-        #btn_s{
-            width:100px;
-        }
-
-        #btn_i {
-            width:125px;
-        }
-        #formbox {
-            margin:auto 0;
-            text-align: center;
-        }
-    </style>
-  </head>
-	<body>
-		<form action="/get">
-      <div id="formbox">
-        <br>
-        <input type="text" name="line1" maxlength="13" style="font-family: 'Courier New'; font-size : 28px; height:40px; width:260px"><br />
-        <br>
-        <input type="text" name="line2" maxlength="13" style="font-family: 'Courier New'; font-size : 28px; height:40px; width:260px"><br />
-        <br>
-        <input type="text" name="line3" maxlength="13" style="font-family: 'Courier New'; font-size : 28px; height:40px; width:260px"><br />
-        <br>
-        <input type="submit" value="flapflap" style="font-size : 35px; height:50px; width:150px">
-      </div>
-		</form><br>
-    <center>
-      Max. 13 Zeichen pro Zeile. <br>
-      Erlaubte Zeichen: 'A'-'Z', '0'-'9', '.', '/', '-', ' '
-    </center>
-	</body>
-</html>
-)rawliteral";
-
-void notFound(AsyncWebServerRequest *request) {
-  request->send(404, "text/plain", "Not found");
+/**
+ * Select (HIGH) or deselect (LOW) each ADL-Pin (i.e. each row)
+ * @param row 0, 1, 2 or 3
+ * @param val HIGH or LOW
+*/
+void setADL(int row, uint8_t val) {
+  digitalWrite(ADL[row], val);
 }
 
-void setupWebserver() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("WiFi Failed!");
-    return;
-  }
-  if(!MDNS.begin("flap")) {
-    Serial.println("Error starting mDNS");
-    return;
-  }
-  Serial.println();
-  Serial.print("IP Address: ");
-  string ipAdress = WiFi.localIP().toString().c_str();
-  cout << ipAdress << endl;
-
-  // Send web page with input fields to client
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
-  });
-
-  // Send a GET request to <ESP_IP>/get?input=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    // GET input value on <ESP_IP>/get?input=<inputMessage>
-    String line1Message = request->getParam("line1")->value();
-    String line2Message = request->getParam("line2")->value();
-    String line3Message = request->getParam("line3")->value();
-    draft[0] = line1Message.c_str();
-    draft[1] = line2Message.c_str();
-    draft[2] = line3Message.c_str();
-    request->redirect("/");
-  });
-  server.onNotFound(notFound);
-  server.begin();
+/**
+ * Select a given column number c.
+ * Automatically un-selects all other columns.
+ * @param col Column-number ([0-n]), n_max=31
+ *            c=0: Modul on the very left (--> ADC=01111 (ADC[3]-ADC[0] are inverted))
+ *            c=31: Modul on the very right (--> ADC=10000)
+*/
+void selectADC(int col) {
+  digitalWrite(ADC[4], (col/16 == 1));  // ADC[4] refers to the EN pin of the Multiplexer-chip, thus it is inverted
+  col = col%16;
+  digitalWrite(ADC[3], (col/8 == 0));
+  col = col%8;
+  digitalWrite(ADC[2], (col/4 == 0));
+  col = col%4;
+  digitalWrite(ADC[1], (col/2 == 0));
+  col = col%2;
+  digitalWrite(ADC[0], (col/1 == 0));
 }
 
-
-
-
-// l: 1, 2, 3 or 4
-// val: HIGH or LOW
-void setADL(int l, uint8_t val) {
-  digitalWrite(ADL[l-1], val);
-}
-
-// c: Column-number ([0-n])
-void selectADC(int c) {
-  digitalWrite(ADC[4], (c/16 == 1));
-  c = c%16;
-  digitalWrite(ADC[3], (c/8 == 0));
-  c = c%8;
-  digitalWrite(ADC[2], (c/4 == 0));
-  c = c%4;
-  digitalWrite(ADC[1], (c/2 == 0));
-  c = c%2;
-  digitalWrite(ADC[0], (c/1 == 0));
-}
-
-void selectAllModules() {
-  for (int i=0; i<=3; i++) {
-    digitalWrite(ADL[i], HIGH);
-  }
-  for (int i=0; i<=4; i++) {
-    digitalWrite(ADC[i], HIGH);
-  }
-}
-
-void deselectAllModules() {
-  for (int i=0; i<=3; i++) {
-    digitalWrite(ADL[i], LOW);
-  }
-  for (int i=0; i<=3; i++) {
-    digitalWrite(ADC[i], LOW);
-  }
-  digitalWrite(ADC[4], HIGH);
-}
-
+/**
+ * Setup ESP-32
+ * [Internal Function, do not change naming]
+*/
 void setup() {
   Serial.begin(115200);
   setupWebserver();
@@ -172,36 +65,36 @@ void setup() {
   for (int i=0; i<=4; i++) {
     pinMode(ADC[i], OUTPUT);
   }
-  pinMode(DATA_0, INPUT);
-  pinMode(DATA_1, INPUT);
-  pinMode(DATA_2, INPUT);
-  pinMode(DATA_3, INPUT);
-  pinMode(DATA_4, INPUT);
-  pinMode(DATA_5, INPUT);
-
-  // Start all modules and then de-select them
-  selectAllModules();
-  digitalWrite(START, HIGH);
-  deselectAllModules();
+  for (int i=0; i<=5; i++) {
+    pinMode(DATA[i], INPUT);
+  }
 }
 
-string getCurrentPosition() {
+/**
+ * @returns String containing binary code of currently selected module
+*/
+string getCurrentBinaryCode() {
   string binaryCode = "";
-  binaryCode.append(to_string(!!!digitalRead(DATA_5)));
-  binaryCode.append(to_string(!!!digitalRead(DATA_4)));
-  binaryCode.append(to_string(!!!digitalRead(DATA_3)));
-  binaryCode.append(to_string(!!!digitalRead(DATA_2)));
-  binaryCode.append(to_string(!!!digitalRead(DATA_1)));
-  binaryCode.append(to_string(!!!digitalRead(DATA_0)));
+  for (int i=5; i>=0; i--) {
+    binaryCode.append(to_string(!digitalRead(DATA[i])));
+  }
   return binaryCode;
 }
 
+/**
+ * @returns Character of currently selected module
+*/
 char getCurrentChar() {
-  string binaryCode = getCurrentPosition();
+  string binaryCode = getCurrentBinaryCode();
   if (binaryCode == "000000") {
     return '+'; // just return some non-existent value
   } 
-  int intCode = (binaryCode[5-0]-'0')*1 + (binaryCode[5-1]-'0')*2 + (binaryCode[5-2]-'0')*4 + (binaryCode[5-3]-'0')*8 + (binaryCode[5-4]-'0')*16 + (binaryCode[5-5]-'0')*32;
+  int intCode = (binaryCode[5]-'0')*1 + (binaryCode[4]-'0')*2 + (binaryCode[3]-'0')*4 + (binaryCode[2]-'0')*8 + (binaryCode[1]-'0')*16 + (binaryCode[0]-'0')*32;
+
+  // intCode 1-26: Characters (A-Z)
+  // intCode 45-57: Numbers (0-9) and dash (-)
+  // intCode 32, 39: Space ( ) and slash (/)
+  // Else: return some non-existent value (+)
   if (intCode >= 1 && intCode <= 26) {
     return char(64 + intCode);
   } else if (intCode >= 45 && intCode <= 57) {
@@ -211,17 +104,22 @@ char getCurrentChar() {
   } else if (intCode == 39) {
     return '/';
   }
-  return '+';
+  return '+'; 
 }
 
+/**
+ * Remove unsupported Characters and, if necessary, add spaces at the end of line
+ * @param text  single row of text, possibly containing unsupported characters (e.g. "fräch!")
+ * @returns     single row of text, with characters replaced an missing spaces (e.g. "fr/ch/     ")
+*/
 string reviseText(string text) {
+  char* supportedCharacters_end = supportedCharacters+size(supportedCharacters);
   for (int j=0; j<size(text); j++) {
     text[j] = toupper(text[j]);
-    if (find(supportedCharacters, supportedCharacters+size(supportedCharacters), text[j]) == (supportedCharacters+size(supportedCharacters))) {
+    if (find(supportedCharacters, supportedCharacters_end, text[j]) == supportedCharacters_end) {
       cout << "Unsupported character: " << text[j] << endl;
       text[j] = '/';
     }
-
   }
   if (size(text) < numOfCols) {
     int missingSpaces = numOfCols - size(text);
@@ -232,51 +130,38 @@ string reviseText(string text) {
   return text;
 }
 
-char getPrecedingCharacter(char myChar) {
-  char* position = find(supportedCharacters, supportedCharacters+size(supportedCharacters), myChar);
-  char precedingChar = *(position-1);
-  if (position == supportedCharacters) {
-    precedingChar = ' ';
-  }
-  return precedingChar;
-}
-
-
+/**
+ * Loop ESP-32
+ * [Internal Function, do not change naming]
+*/
 void loop() {
-  string oldDraft[] = {"", "", ""};
   string text[] = {"", "", ""};
-  string lastOutputChars[numOfRows * numOfCols];
   int isCorrect[numOfRows * numOfCols];
+  fill_n(isCorrect, numOfRows * numOfCols, 10);
 
   // Iterate through the Matrix (Row-wise)
   while(1) {
-    if (oldDraft[0] != draft[0] || oldDraft[1] != draft[1] || oldDraft[2] != draft[2]) {
-      text[0] = reviseText(draft[0]);
-      text[1] = reviseText(draft[1]);
-      text[2] = reviseText(draft[2]);
-      oldDraft[0] = draft[0];
-      oldDraft[1] = draft[1];
-      oldDraft[2] = draft[2];
-      for (int i=0; i<numOfRows * numOfCols; i++) {
-        lastOutputChars[i] = "";
-        isCorrect[i] = 0;
-      }
+    if (isDraftChange(numOfRows-1)) {
+      text[0] = reviseText(getDraft(0));
+      text[1] = reviseText(getDraft(1));
+      text[2] = reviseText(getDraft(2));
+      fill_n(isCorrect, numOfRows * numOfCols, 0);
     }
 
     for (int i=0; i<numOfRows; i++) {
       string line = text[i];
       for (int j=0; j<numOfCols; j++) {
+        // Correct char needs to be recognized in 10 iterations in a row to be valid
         if (isCorrect[numOfCols*i + j] < 10) {
           char myChar = line[j];
-          char precedingChar = getPrecedingCharacter(myChar);
 
-          // Try to stop the motor
+          // Try to stop the motor by de-activating START and selecting the module
           digitalWrite(START, LOW);
           usleep(5);
           selectADC(j);
-          setADL(i+1, HIGH);
+          setADL(i, HIGH);
 
-          // Stop if myChar is found
+          // Stop if myChar is found (set START to LOW)
           char currentChar = getCurrentChar();
           if(currentChar == myChar) {
             digitalWrite(START, LOW);
@@ -288,14 +173,10 @@ void loop() {
 
           // Un-select the module, so it continues turning (if character was not found)
           usleep(5);
-          setADL(i+1, LOW);
+          setADL(i, LOW);
           selectADC(31);
         }
       }
     } 
   }
 }
-
-//   chrono::steady_clock::time_point begin = chrono::steady_clock::now();
-//   chrono::steady_clock::time_point end = chrono::steady_clock::now();
-//   cout << endl << "  Waited for valid position for " << chrono::duration_cast<chrono::microseconds>(end - begin).count() << "[µs]" << endl;
